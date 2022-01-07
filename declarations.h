d56a29f7b5c7fe1949d403bb7fbd1cbba30d149a
taimoorzaeem <87592739+taimoorzaeem@users.noreply.github.com>
2022-01-08 02:19:19 +0500

    Added Include guards in declarations.h

--- declarations.h
+++ declarations.h
@@ -1,5 +1,10 @@
+#ifndef __DECLARATIONS_H // Include guards
+#define __DECLARATIONS_H
+
 void die(const char *message);
 char *keysignature(char *key, int keylen);
 char *readkeyfile(FILE *fp);
 void check_sign(FILE *fp, char *key_sign, char *file_op);
 FILE *run_algorithm(FILE *fp, char *key, int keylen, char *file_op);
+
+#endif
